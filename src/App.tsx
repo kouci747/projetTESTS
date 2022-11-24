bf5ffc67f16ee4394c46a19cd3437aa6a8a5b57c
kouci747 <koceila.arhab@gmail.com>
2022-11-24 12:40:44 +0100

    commit pour ofsen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import TextField from './components/InputComponents/TextField'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <TextField/>
   )
 }
 
---
+++ src/components/InputComponents/TextField.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TextField() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <label>Cap de la piste</label>
+      <input  placeholder='enter runway heading'></input>
+    </div>
+  )
+}
