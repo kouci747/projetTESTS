ff7f41ccab664a0cb5d674d6432c41c8e8589df6
kouci747 <koceila.arhab@gmail.com>
2022-12-01 10:29:08 +0100

    push kouci 1 décembre

--- cypress/e2e/TakeOff.cy.ts
+++ cypress/e2e/TakeOff.cy.ts
@@ -20,6 +20,8 @@ describe("Testing data entering", () => {
     cy.get("input[id='Force du vent']").type("0").should("have.value", "0");
   });
 
+
+
   it("Checks 'Aucune données saisie' doesn't exist in the ", () => {
     cy.contains("Aucune données saisie").should("not.exist");
   });
@@ -29,3 +31,45 @@ describe("Testing data entering", () => {
     cy.contains("Good to go").should("exist");
   });
 });
+
+describe("Testing data entering 2: tail wind avec décollage possible", () => {
+  it("Entering data", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("input[id='Cap de la piste']").type("090").should("have.value", "090");
+    cy.get("input[id='Direction du vent']").type("270").should("have.value", "270");
+    cy.get("input[id='Force du vent']").type("0").should("have.value", "0");
+  });
+
+
+
+  it("Checks 'Aucune données saisie' doesn't exist in the ", () => {
+    cy.contains("Aucune données saisie").should("not.exist");
+  });
+
+  it("Checks Tail Wind exists in results", () => {
+    cy.contains("Tail Wind").should("exist");
+    cy.contains("Good to go").should("exist");
+  });
+});
+
+describe("Testing data entering 2: tail wind avec décollage impossible", () => {
+  it("Entering data", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("input[id='Force du vent']").type("11").should("have.value", "11");
+    cy.get("input[id='Cap de la piste']").type("090").should("have.value", "090");
+    cy.get("input[id='Direction du vent']").type("270").should("have.value", "270");
+  });
+
+
+
+  it("Checks 'Aucune données saisie' doesn't exist in the ", () => {
+    cy.contains("Aucune données saisie").should("not.exist");
+  });
+
+  it("Checks Tail Wind exists in results", () => {
+    cy.contains("Good to go").should("not.exist");
+    cy.contains("Tail Wind").should("exist");
+  });
+});
+
+
